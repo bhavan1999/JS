e5f7b5f9de1ea675445b8481bcdab9265e7d26dc
bhavan1999 <bhavankatre2408@gmail.com>
2022-04-13 18:15:48 +0530

    Swapping

---
+++ JS6.js
@@ -0,0 +1,18 @@
+let x=2;
+let y=3;
+//  long hands on
+
+const temp=x;
+x=y;
+y=temp;
+
+console.log(x);
+console.log(y);
+
+let a = "BHAVAN";
+let b = "KATRE";
+
+// Short hands on
+[a,b] = [b,a];
+console.log(a);
+console.log(b);
