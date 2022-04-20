ecde1bbc59626a0d65b546350d8224a0fa0a66a0
bhavan1999 <bhavankatre2408@gmail.com>
2022-04-20 21:36:28 +0530

    Array Join Method-2

--- ArrayJoin.js
+++ ArrayJoin.js
@@ -4,3 +4,12 @@ const v1 = ["Bhavan","Katre"];
 const v2 = ["Hello","World"];
 const v3 = ["Good","Bye"];
 console.log(v1.concat(v2,v3));
+
+// 2. Spread Operator(...)
+
+const v4 = ["Bhavan","Katre"];
+const v5 = ["Hello","World"];
+const v6 = ["Good","Bye"];
+console.log(...v1,...v2,...v3);
+
+// 
