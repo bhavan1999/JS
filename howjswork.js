dad29f7cde8b4fd77e27f45a48be1fd1a2d397cb
bhavan1999 <bhavankatre2408@gmail.com>
2022-04-22 22:32:09 +0530

    JS Working

--- howjswork.js
+++ howjswork.js
@@ -23,3 +23,14 @@ var sq2 = sq(n);
 var sq4 = sq(4);
 console.log(sq2);
 console.log(sq4);
+
+// first GEC{Global Execution Context} is Created
+//  Memory Component{Variable Enviroment}           Code Component{Thread of the Execution}
+// n : undefined                                    ----> After Running Memory Phase Code Phase Will Work
+// sq : sq{....}(Stores the Whole Code)                  n : 2   {line no vs code 17}
+//  sq2 : undefined                                      function invocation will happens {line no vs code 22 and 18,19,20,21 will not not executed}
+//  sq4 : undefined                                      Again new EC{Execution context will be created}
+//                                                       Memory Component{Variable Enviroment}           Code Component{Thread of the Execution}
+//                                                       num : undefined                                  num : 2
+//                                                       ans : undefined                                  ans : 4
+//  Call Stack :- The call stack is used by JavaScript to keep track of multiple function calls
