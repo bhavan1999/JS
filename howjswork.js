0e7dc331c3e5f285366c9c5447b75f4d0910480a
bhavan1999 <bhavankatre2408@gmail.com>
2022-04-22 22:01:40 +0530

    JS Working

--- howjswork.js
+++ howjswork.js
@@ -1,3 +1,5 @@
+//  [1.] How JavaScript Works?
+
 //  "Everything in JS happens inside an Execution Context. "
 
 // Execution Context content of two components :-
@@ -7,4 +9,9 @@
 // fn() : {....};
 // 2. Code Component {Thread of the Execution} :- This the place where code is get executed one at a line.
 // "JavaScript is a synchronous single threaded language" [Synchronous mean at a specific order]
+
+// ---------------------------------------------------------------------
+
+
+
 
