0d58141d6ab199af169ba0efc2747d5bdf60593f
bhavan1999 <bhavankatre2408@gmail.com>
2022-04-22 21:59:26 +0530

    JS Working

---
+++ howjswork.js
@@ -0,0 +1,10 @@
+//  "Everything in JS happens inside an Execution Context. "
+
+// Execution Context content of two components :-
+// 1. Memory Component {Variable Enviroment} :- All Variables and Functions are stored in the form of Key Value Pairs.
+//  Ex:- a:1;
+// b:2;
+// fn() : {....};
+// 2. Code Component {Thread of the Execution} :- This the place where code is get executed one at a line.
+// "JavaScript is a synchronous single threaded language" [Synchronous mean at a specific order]
+
